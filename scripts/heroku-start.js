edd9f5c7fc4b69f51fbc3654fda67a052b59f904
Ralph <ralphestor30@gmail.com>
2023-08-16 00:40:41 +0800

    revert changes trying to resolve heroku errors

--- scripts/heroku-start.js
+++ scripts/heroku-start.js
@@ -15,7 +15,7 @@ app.get("/", function (req, res) {
 
 // Any Page Redirects to the build in assets folder index.html that will load the react app
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../build/", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "build/index.html"));
 });
 
 app.listen(port, () => {
